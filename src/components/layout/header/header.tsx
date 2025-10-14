8e94574a8a05168c8965375a461aeeb82b5ea2e1
Ivanna-Kutsovol <st6815720@stud.duikt.edu.ua>
2025-10-15 00:59:00 +0200

    feat(header): make header responsive

---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React, {useState} from "react";
+import stl from "./header.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+
+import Logo from "../../../../public/logo.svg";
+import Gradient from "../../../../public/gradientMobileMenu.svg";
+
+export const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <header className={`${stl.header} ${isOpen ? stl.open : ""}`}>
+            <Image src={Logo} alt="logo" width={99} height={45}/>
+                <button className={`${stl.header__burger} ${isOpen ? stl.open : ""}`}
+                    onClick={() => setIsOpen(!isOpen)}>
+                    <span className={stl.bar}/>
+                    <span className={stl.bar}/>
+                </button>
+            <nav className={`${stl.header__nav} ${isOpen ? stl.open : ""}`}>
+                <Link className={stl.header__link} href="/">Home</Link>
+                <Link className={stl.header__link} href="/#about">About</Link>
+                <Link className={stl.header__link} href="/#contact">Experience</Link>
+                <Link className={stl.header__link} href="/#contact">Skills</Link>
+                <Link className={stl.header__link} href="/#contact">Portfolio</Link>
+                <Image className={stl.header__gradient} src={Gradient} alt="gradient" width={333} height={333}/>
+            </nav>
+        </header>
+    );
+};
