95840074c1c5d4b7f49e942a3c1a0edccceeef2f
Ivanna-Kutsovol <st6815720@stud.duikt.edu.ua>
2025-11-08 21:31:16 +0100

    feat(experience): update text content and adjust text size

--- src/app/experience/experience.tsx
+++ src/app/experience/experience.tsx
@@ -4,28 +4,28 @@ import Link from "next/link";
 import Image from "next/image";
 
 const array = [{
-    title: 'CIB on the Mobile',
-    description: 'Take your client onboard seamlessly by our amazing tool of digital onboard process.',
+    title: 'Frontend Dev',
+    description: 'Developing responsive, pixel-perfect websites using Next.js, React, and SCSS. Focused on clean UI, animations, and reusable components.',
     icon: "/experience/icon1.svg",
-    button: "/#contact",
+    button: "/#project",
 },
 {
-    title: 'CIB on the Mobile',
-    description: 'Take your client onboard seamlessly by our amazing tool of digital onboard process.',
+    title: 'UI/UX Design',
+    description: 'Creating user-centered designs from scratch. Worked on layouts, color systems, and interactions — including the design for my Toy House project.',
     icon: "/experience/icon2.svg",
-    button: "/#contact",
+    button: "https://www.figma.com/design/1bUeWh5a6nxUUrsmvl6uHN/toy-house?node-id=0-1&t=WXpdsEbiQGns8O8y-1",
 },
 {
-    title: 'CIB on the Mobile',
-    description: 'Take your client onboard seamlessly by our amazing tool of digital onboard process.',
+    title: 'API & Backend',
+    description: 'Experience integrating RESTful APIs and backend services using TypeScript and Spring Boot. Implemented authentication, role-based access, and database connections for projects.',
     icon: "/experience/icon3.svg",
-    button: "/#contact",
+    button: "https://github.com/Ivanna-Kutsovol/metamix",
 },
 {
-    title: 'CIB on the Mobile',
-    description: 'Take your client onboard seamlessly by our amazing tool of digital onboard process.',
+    title: 'WordPress Dev',
+    description: 'I create and customize WordPress websites — from installing themes and plugins to building fully custom layouts using Elementor and custom CSS.',
     icon: "/experience/icon4.svg",
-    button: "/#contact",
+    button: "",
 }]
 
 const Experience = () => {
