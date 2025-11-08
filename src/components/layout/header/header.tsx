de991b7b833aaaef7f46ecb5206da54332d4fca8
Ivanna-Kutsovol <st6815720@stud.duikt.edu.ua>
2025-11-08 21:35:30 +0100

    fix(header): update link text to "Project"

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -22,7 +22,7 @@ export const Header = () => {
                 <Link className={stl.header__link} href="/#about">About</Link>
                 <Link className={stl.header__link} href="/#experience">Experience</Link>
                 <Link className={stl.header__link} href="/#skills">Skills</Link>
-                <Link className={stl.header__link} href="/#project">Portfolio</Link>
+                <Link className={stl.header__link} href="/#project">Project</Link>
                 <Image className={stl.header__gradient} src={Gradient} alt="gradient" width={333} height={333}/>
             </nav>
         </header>
